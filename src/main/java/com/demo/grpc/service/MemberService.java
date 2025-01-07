5df0c3ae09cdb1aeb4ebe101e93ff0fa2db80a60
wlsdks <wlsdks12@naver.com>
2025-01-08 00:01:07 +0900

    :recycle: 메서드에 트랜잭션 추가, 스프링 버전 최신화

--- src/main/java/com/demo/grpc/service/MemberService.java
+++ src/main/java/com/demo/grpc/service/MemberService.java
@@ -7,6 +7,7 @@ import com.demo.grpc.mapper.MemberMapper;
 import com.demo.grpc.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
@@ -15,6 +16,7 @@ public class MemberService {
     private final MemberRepository memberRepository;
     private final MemberMapper memberMapper;
 
+    @Transactional
     public ResponseMemberDTO createMember(MemberSignUpRequestDTO memberDTO) {
         Member member = memberMapper.dtoToEntity(memberDTO);
         Member savedMember = memberRepository.save(member);
